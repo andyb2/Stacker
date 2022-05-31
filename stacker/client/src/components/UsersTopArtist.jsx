f8fdfd306515573256113d48ce028f3ad3671a0a
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 12:37:02 -0400

    inline styling for img tag moved to modular UsersTopArtist.css file

--- stacker/client/src/components/UsersTopArtist.jsx
+++ stacker/client/src/components/UsersTopArtist.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import './styles/UsersTopArtist.css';
 const UsersTopArtist = () => {
     const topArtists = useSelector(state => state.spotify.topArtist);
-    console.log(topArtists)
+    
     return (
             <div className="top-artist-conatiner">
                 <h1 className="header-top-artist">Top Artists</h1>
@@ -12,7 +12,7 @@ const UsersTopArtist = () => {
                         return (
                             <a href={artist.external_urls.spotify} target="_blank" rel="noreferrer">
                                 <div className="artist-card">
-                                        <img src={`${artist.images[0].url}`} alt="album cover" style={{height: '150px', marginBottom: '1rem', borderRadius: '2px'}}/>
+                                        <img src={`${artist.images[0].url}`} className="artist-image" alt="album cover" />
                                         <div className="artist-name">{artist.name}</div>
                                 </div>
                             </a>
