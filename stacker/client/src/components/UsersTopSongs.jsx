bb9abdbd58582e1dda3944f935ef17c4f67aabce
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 11:50:09 -0400

    removed unused component that was rendering a custom top three design, also removed code relating to the TopThree component

--- stacker/client/src/components/UsersTopSongs.jsx
+++ stacker/client/src/components/UsersTopSongs.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
+import "./styles/UsersTopSongs.css";
+import { converMStoMinutes } from "../utils/index"
 
 const UsersTopSongs = () => {
     const topSongs = useSelector((state) => state.spotify.topSongs);
-    console.log(`TOP SONGS:`, topSongs)
+
     return (
-        <div style={{color: 'white'}}>
-            { topSongs &&
-                 topSongs.items.map((songs) => {
-                    return (
-                        <div>
-                            <div>{songs.artists[0].name}</div>
-                            <div>{songs.name}</div>
-                        </div>
-                    )   
-                })    
-            }
+        <div className="songs-parent">
+            <h1 className="header-top-songs">Top Songs</h1>
+            <div className="songs-container">
+                
+                { topSongs &&
+                    topSongs.items.map((songs) => {
+                        return (
+                            <div className="song-row" key={`${songs.id}`}>
+                                <div className="song-block">
+                                    <img src={songs.album.images[0].url} className="songs-image" alt="album cover" />
+                                    <div className="song-info">
+                                        <div className="song-name">{songs.name}</div>
+                                        <div className="song-artist">{songs.artists[0].name}</div>
+                                    </div>
+                                </div>
+                                <div className="song-time-length">
+                                    <div>{converMStoMinutes(songs.duration_ms)}</div>
+                                </div>
+                            </div>
+                        )
+                    })  
+                }
+            </div>
         </div>
     )
 }
