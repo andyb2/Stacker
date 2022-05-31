8a834fcfc56693ec1e77ce0cd7fe953ffe05e409
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 14:29:31 -0400

    removed inline styling from all elements and moved to modular css file

--- stacker/client/src/components/RecentlyPlayed.jsx
+++ stacker/client/src/components/RecentlyPlayed.jsx
@@ -14,15 +14,15 @@ const RecentlyPLayed = () => {
                 recent.items.map(({ track }, idx) => {
                     return (
                         <div className='recent-row' key={`${track.id} ${idx}`}>
-                            <div style={{display: 'flex', gap: '1rem'}}>
-                                <img src={`${track.album.images[0].url}`} alt='album cover' style={{height: '50px'}}/>
-                                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flexShrink: '4'}}>
-                                    <div style={{color: 'white', fontSize: '16px'}}>{track.name}</div>
-                                    <div style={{color: 'grey', fontSize: '13px'}}>{track.artists[0].name}</div>
+                            <div className='recent-song-info'>
+                                <img className='recent-song-image' src={`${track.album.images[0].url}`} alt='album cover'/>
+                                <div className='recent-song-artist-name'>
+                                    <div className='recent-track-name'>{track.name}</div>
+                                    <div className='recent-track-artist'>{track.artists[0].name}</div>
                                 </div>
                             </div>
-                            <div style={{display: 'flex', alignItems: 'center', paddingRight: '0.5rem'}}>
-                                <div style={{color: 'grey', fontSize: '16px'}}>{convertMStoMinutes(track.duration_ms)}</div>
+                            <div className='recent-song-duration' >
+                                <div>{convertMStoMinutes(track.duration_ms)}</div>
                             </div>
                         </div>
                     )
