67e8c147f28ba602370823f788b9d52293d2ee95
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 11:40:25 -0400

    component css updated to have a max width

--- stacker/client/src/components/RecentlyPlayed.jsx
+++ stacker/client/src/components/RecentlyPlayed.jsx
@@ -1,14 +1,11 @@
 import './styles/RecentlyPlayed.css';
 import { useSelector } from "react-redux";
+import { converMStoMinutes } from '../utils';
 
 const RecentlyPLayed = () => {
     const recent = useSelector((state) => state.spotify.recent);
     
-    const converMStoMinutes = (ms) => {
-        const minutes = Math.floor(ms / 60000);
-        const seconds = Math.floor((ms % 60000) / 1000).toFixed(0);
-        return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`
-      }
+    
 
     return (
         <div className='recent-container'>
