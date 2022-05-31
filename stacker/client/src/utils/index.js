8a424696511974145efe1f4de34fe0bd6bc8d5f9
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 13:50:04 -0400

    converMStoMinutes function name had a type... convert was missing a t

--- stacker/client/src/utils/index.js
+++ stacker/client/src/utils/index.js
@@ -8,7 +8,7 @@ export const getParam = () => {
     return hashParams;
 }
 
-export const converMStoMinutes = (ms) => {
+export const convertMStoMinutes = (ms) => {
     const minutes = Math.floor(ms / 60000);
     const seconds = Math.floor((ms % 60000) / 1000).toFixed(0);
     return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`
