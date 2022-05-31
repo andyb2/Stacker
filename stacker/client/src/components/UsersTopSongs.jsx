d835a7a6e045799057ba64db9657f6285f984485
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 13:49:14 -0400

    converMStoMinutes import and function call renamed because of typo... new name is convertMStoMinutes

--- stacker/client/src/components/UsersTopSongs.jsx
+++ stacker/client/src/components/UsersTopSongs.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import "./styles/UsersTopSongs.css";
-import { converMStoMinutes } from "../utils/index"
+import { convertMStoMinutes } from "../utils/index"
 
 const UsersTopSongs = () => {
     const topSongs = useSelector((state) => state.spotify.topSongs);
@@ -11,18 +11,18 @@ const UsersTopSongs = () => {
             <div className="songs-container">
                 
                 { topSongs &&
-                    topSongs.items.map((songs) => {
-                        return (
-                            <div className="song-row" key={`${songs.id}`}>
+                    topSongs.items.map(({id, album, name, artists, duration_ms}) => {
+                        return (    
+                            <div className="song-row" key={`${id}`}>
                                 <div className="song-block">
-                                    <img src={songs.album.images[0].url} className="songs-image" alt="album cover" />
+                                    <img src={album.images[0].url} className="songs-image" alt="album cover" />
                                     <div className="song-info">
-                                        <div className="song-name">{songs.name}</div>
-                                        <div className="song-artist">{songs.artists[0].name}</div>
+                                        <div className="song-name">{name}</div>
+                                        <div className="song-artist">{artists[0].name}</div>
                                     </div>
                                 </div>
                                 <div className="song-time-length">
-                                    <div>{converMStoMinutes(songs.duration_ms)}</div>
+                                    <div>{convertMStoMinutes(duration_ms)}</div>
                                 </div>
                             </div>
                         )
