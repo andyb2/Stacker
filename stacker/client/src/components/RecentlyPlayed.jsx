ffccaec451d16783f5dc5b3c3442e095ddaf8cca
Andrew Boyle <andrewboyle@ab.local>
2022-05-31 13:58:03 -0400

    recent.items.map now destructures the get request for recently played songs

--- stacker/client/src/components/RecentlyPlayed.jsx
+++ stacker/client/src/components/RecentlyPlayed.jsx
@@ -11,18 +11,18 @@ const RecentlyPLayed = () => {
         <div className='recent-container'>
             <h1 style={{color: 'white'}}>Recently Played</h1>
             { recent && 
-                recent.items.map((songs, idx) => {
+                recent.items.map(({ track }, idx) => {
                     return (
-                        <div className='recent-row' key={`${songs.track.id} ${idx}`}>
+                        <div className='recent-row' key={`${track.id} ${idx}`}>
                             <div style={{display: 'flex', gap: '1rem'}}>
-                                <img src={`${songs.track.album.images[0].url}`} alt='album cover' style={{height: '50px'}}/>
+                                <img src={`${track.album.images[0].url}`} alt='album cover' style={{height: '50px'}}/>
                                 <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flexShrink: '4'}}>
-                                    <div style={{color: 'white', fontSize: '16px'}}>{songs.track.name}</div>
-                                    <div style={{color: 'grey', fontSize: '13px'}}>{songs.track.artists[0].name}</div>
+                                    <div style={{color: 'white', fontSize: '16px'}}>{track.name}</div>
+                                    <div style={{color: 'grey', fontSize: '13px'}}>{track.artists[0].name}</div>
                                 </div>
                             </div>
                             <div style={{display: 'flex', alignItems: 'center', paddingRight: '0.5rem'}}>
-                                <div style={{color: 'grey', fontSize: '16px'}}>{convertMStoMinutes(songs.track.duration_ms)}</div>
+                                <div style={{color: 'grey', fontSize: '16px'}}>{convertMStoMinutes(track.duration_ms)}</div>
                             </div>
                         </div>
                     )
