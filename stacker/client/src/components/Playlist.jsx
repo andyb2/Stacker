2defad1c1dea1e6d2cea44bd7ab034e625dae638
Andrew Boyle <andrewboyle@ab.local>
2022-06-02 09:45:30 -0400

    removed inline styling and added color-grey class

--- stacker/client/src/components/Playlist.jsx
+++ stacker/client/src/components/Playlist.jsx
@@ -1,15 +1,17 @@
 import { useSelector } from "react-redux";
+import './styles/Playlist.css';
+import PlaylistImage from "./PlaylistImage";
 
 const Playlist = () => {
     const playlists = useSelector((state) => state.spotify.playlists)
     return (
         <div>
             {playlists &&
-                playlists.items.map(({images, name, tracks}) => {
+                playlists.items.map(({ images, name }) => {
                     return (
                         <div>
-                            { images.length ? <img src={images[0].url} alt="playlist art" /> : 'none' }
-                            <div style={{color: '#DEDEDE'}}>{name}</div>
+                            { images.length ? <img className="playlist-image" src={images[0].url} alt="playlist art" /> : <PlaylistImage /> }
+                            <div className='color-grey'>{name}</div>
                         </div>
                     )
                 })
