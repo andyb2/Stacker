c0307f700c78e0643d1635328ebbbbdc98a67d2f
Andrew Boyle <andrewboyle@ab.local>
2022-06-02 12:16:35 -0400

    line 11 -> id added to mapped object destructuring. line 13 -> key added to anchor tag for creating unique ids

--- stacker/client/src/components/UsersTopArtist.jsx
+++ stacker/client/src/components/UsersTopArtist.jsx
@@ -2,15 +2,15 @@ import { useSelector } from "react-redux";
 import './styles/UsersTopArtist.css';
 const UsersTopArtist = () => {
     const topArtists = useSelector(state => state.spotify.topArtist);
-    
+
     return (
             <div className="top-artist-conatiner">
                 <h1 className="header-top-artist">Top Artists</h1>
                 <div className="artist-main">
                 { topArtists && 
-                    topArtists.items.map(({ external_urls, images, name }) => {
+                    topArtists.items.map(({ external_urls, images, name, id }, idx) => {
                         return (
-                            <a href={external_urls.spotify} target="_blank" rel="noreferrer">
+                            <a key={`${id}${idx}`} href={external_urls.spotify} target="_blank" rel="noreferrer">
                                 <div className="artist-card">
                                     <img src={`${images[0].url}`} className="artist-image" alt="album cover" />
                                     <div className="artist-name">{name}</div>
