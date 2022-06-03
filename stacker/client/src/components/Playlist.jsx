e548c5d44dadd17bccd2ea2a4ad76c4b44f3811a
Andrew Boyle <andrewboyle@ab.local>
2022-06-03 08:44:35 -0400

    playlist main class added as a child of playlist container. the play list main is used as a grid to render the cards. There was issues with display flex and using the ::after psuedo element to generate filler cards at the end of the row when there wasn't enough content to complete the line. Grid makes it eaiser to fill those gaps.

--- stacker/client/src/components/Playlist.jsx
+++ stacker/client/src/components/Playlist.jsx
@@ -1,21 +1,26 @@
 import { useSelector } from "react-redux";
 import './styles/Playlist.css';
 import PlaylistImage from "./PlaylistImage";
+import { useEffect } from "react";
 
 const Playlist = () => {
-    const playlists = useSelector((state) => state.spotify.playlists)
+    const playlists = useSelector((state) => state.spotify.playlists);
+    
     return (
-        <div>
-            {playlists &&
-                playlists.items.map(({ images, name }) => {
-                    return (
-                        <div>
-                            { images.length ? <img className="playlist-image" src={images[0].url} alt="playlist art" /> : <PlaylistImage /> }
-                            <div className='color-grey'>{name}</div>
-                        </div>
-                    )
-                })
-            }
+        <div className='playlist-container'>
+            <h1 className='spotify-color-white playlist-header'>Playlists</h1>
+            <div className="playlist-main">
+                {playlists &&
+                    playlists.items.map(({ images, name }) => {
+                        return (
+                            <div className="playlist-card">
+                                { images.length ? <img className="playlist-image" src={images[0].url} alt="playlist art" /> : <PlaylistImage /> }
+                                <div className='playlist-name color-grey'>{name}</div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
