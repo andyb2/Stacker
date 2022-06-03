076b1ab5b64fe628e99208f0c4808e17a14d62b2
Andrew Boyle <andrewboyle@ab.local>
2022-06-03 08:25:31 -0400

    NavLink classes now hold a function with the parameter isActive that returns an active class if true. This will highlight an active link that was pressed in the sidebar. the background color will highlight grey to show where the user is in the application

--- stacker/client/src/components/Sidebar.jsx
+++ stacker/client/src/components/Sidebar.jsx
@@ -16,19 +16,19 @@ const Sidebar = () => {
                 <SpotifyLogo />
             </div>
             <div className='nav-buttons'>
-                <NavLink to='/' className='nav-style'>
+                <NavLink to='/' className={({ isActive }) => `nav-style ${isActive ? 'nav-active' : ''}`}>
                     <div className='icon-holder'>
                         <FontAwesomeIcon icon={faUser} size={'xl'} color={'grey'} />
                         <p className='sidebar-titles'>Profile</p>
                     </div>
                 </NavLink>
-                <NavLink to='/topArtist' className='nav-style'>
+                <NavLink to='/topArtist' className={({ isActive }) => `nav-style ${isActive ? 'nav-active' : ''}`}>
                     <div className='icon-holder'>
                         <FontAwesomeIcon icon={faMusic} size={'xl'} color={'grey'} />
                         <p className='sidebar-titles'>Top Artists</p>
                     </div>
                 </NavLink>
-                <NavLink to='topSongs' className='nav-style'>
+                <NavLink to='/topSongs' className={({ isActive }) => `nav-style ${isActive ? 'nav-active' : ''}`}>
                     <div className='icon-holder'>
                         <FontAwesomeIcon icon={faMicrophoneLines} size={'xl'} color={'grey'} />
                         <p className='sidebar-titles'>Top Songs</p>
@@ -38,7 +38,7 @@ const Sidebar = () => {
                     <FontAwesomeIcon icon={faClockRotateLeft} size={'xl'} color={'grey'} />
                     <p className='sidebar-titles'>Recent</p>
                 </div>
-                <NavLink to="/playlists" className='nav-style'>
+                <NavLink to="/playlists" className={({ isActive }) => `nav-style ${isActive ? 'nav-active' : ''}`}>
                 <div className='icon-holder'>
                     <FontAwesomeIcon icon={faBarsStaggered} size={'xl'} color={'grey'} />
                     <p className='sidebar-titles'>Playlist</p>
