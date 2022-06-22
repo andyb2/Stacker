28c8ed924b7e58d821e27edc734bdef64f4ed4f6
Andrew Boyle <andrewboyle@ab.local>
2022-06-21 21:32:37 -0400

    typo with process.env.port

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const { join } = require('path');
 const cors = require('cors')
-const PORT = process.env.port || 8888;
+const PORT = process.env.PORT || 8888;
 const app = express();
 
 app.use(cors());
