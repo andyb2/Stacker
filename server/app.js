bd0d1c75ddb88794b0d4838ab70d4da2d4b5890f
Andrew Boyle <a.michael.boyle@gmail.com>
2024-06-02 21:10:55 -0400

    app stopped working.. fixed api call issues and some sizing of images/oadding

--- server/app.js
+++ server/app.js
@@ -2,22 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 const PORT = process.env.PORT || 8888;
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../client/build')))
-    .use(cookieParser());
+app
+  .use(express.static(path.resolve(__dirname, '../client/build')))
+  .use(cookieParser());
 
-app.use("/spotify", require("./routes/spotify"));
+app.use('/spotify', require('./routes/spotify'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on PORT: ${PORT}`)
+  console.log(`server is running on PORT: ${PORT}`);
 });
--- server/routes/spotify/index.js
+++ server/routes/spotify/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const querystring = require('node:querystring');
 const request = require('request');
 const stateKey = 'spotify_auth_state';
-const scopes = 'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read playlist-read-private playlist-read-collaborative';
+const scopes =
+  'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read playlist-read-private playlist-read-collaborative';
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/spotify/callback';
+let REDIRECT_URI =
+  process.env.REDIRECT_URI || 'http://localhost:8888/spotify/callback';
 let URI = process.env.URI || 'http://localhost:3000';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -15,34 +17,39 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const generateRandomString = (length) => {
-  let string = ''
-  let allowable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let string = '';
+  let allowable =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < length; i++) {
-    string += allowable[Math.floor(Math.random() * allowable.length)]
+    string += allowable[Math.floor(Math.random() * allowable.length)];
   }
-  return string
-}
+  return string;
+};
 router.get('/login', (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: CLIENT_ID,
-      scope: scopes,
-      redirect_uri: REDIRECT_URI,
-      state: state
-    }));
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope: scopes,
+        redirect_uri: REDIRECT_URI,
+        state: state,
+      })
+  );
 });
 router.get('/callback', (req, res) => {
   const code = req.query.code || null;
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[stateKey] : null;
   if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
+    res.redirect(
+      '/#' +
+        querystring.stringify({
+          error: 'state_mismatch',
+        })
+    );
   } else {
     res.clearCookie(stateKey);
     const authOptions = {
@@ -50,27 +57,33 @@ router.get('/callback', (req, res) => {
       form: {
         code: code,
         redirect_uri: REDIRECT_URI,
-        grant_type: 'authorization_code'
+        grant_type: 'authorization_code',
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        Authorization:
+          'Basic ' +
+          new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
       },
-      json: true
+      json: true,
     };
     request.post(authOptions, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         const access_token = body.access_token,
           refresh_token = body.refresh_token;
-        res.redirect(`${URI}/#` +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+        res.redirect(
+          `${URI}/#` +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
       } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
+        res.redirect(
+          '/#' +
+            querystring.stringify({
+              error: 'invalid_token',
+            })
+        );
       }
     });
   }
@@ -80,22 +93,24 @@ router.post('/refresh_token', (req, res) => {
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: {
-      'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+      Authorization:
+        'Basic ' +
+        new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
     },
     form: {
       grant_type: 'refresh_token',
-      refresh_token: refresh_token
+      refresh_token: refresh_token,
     },
-    json: true
+    json: true,
   };
-    request.post(authOptions, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const access_token = body.access_token;
-        res.send({
-          'access_token': access_token
-        });
-      }
-    });
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+      res.send({
+        access_token: access_token,
+      });
+    }
   });
+});
 
-module.exports = router
+module.exports = router;
